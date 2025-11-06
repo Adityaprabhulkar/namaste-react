ac3ad8ec47a9ffc37fa8a0e3ee91fb8bbaae0b3f
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-07 00:30:30 +0530

    Updated App.js and added package files

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// <div id="parent">
-//     <div id="child">
-//         <h1>I am h1 Tag</h1>
-//         <h2>I'am h2 tag</h2>
-//     </div>
-// </div>
-// <div id="parent2">
-//     <div id="child">
-//         <h1>I am h1 Tag</h1>
-//         <h2>I'am h2 tag</h2>
-//     </div>
-// </div>
-
-const parent = React.createElement("div", {id: "parent" },
-    [        React.createElement("div", {id: "child" }, [
-                React.createElement("h1",{}, "I'am h1 tag" ),
-                React.createElement("h2",{}, "I'am h2 tag" )]
-    ),       React.createElement("div", {id: "child2" }, [
-                React.createElement("h1",{}, "I'am h1 tag" ),
-                React.createElement("h2",{}, "I'am h2 tag" )]
-    )]
-
- );
-
- //JSX
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", }, [
+    React.createElement("h1", {}, "This is Namaste React🚀"),
+    React.createElement("h2", {}, "fcsgvdhvhxgyegyfbjsdbcjkegydczhbxcjkbds"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'am h1 tag"),
+    React.createElement("h2", {}, "I'am h2 tag"),
+  ]),
+])
 
 
 console.log(parent);  //object
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# namaste-react
-I have started from this react session from namaste-react by akshay saini
+# namaste-react 🚀
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Alogorithm = written by C++
+- Caching - Faster Builds
+- Iamge Optimization
+- Minification
+- Bundling
+- Compressing  
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unsaved code
+- Different dev and prod bundles
