b2620c9b77c1d4128624cf27b7a29f6a7dfa6876
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-09 02:19:40 +0530

    Creating Food App Using React

--- App.js
+++ App.js
@@ -2,31 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// React Element => Object => HTMLElement(render)
-
-const elem = <span>Hello Jee</span>
 
 
 
-// React Function Component
-
-// Component Composition
-
-const Title = ()=> (
-  <h1 className="head" tabIndex="5">
-     
-    Namaste React using JSX🚀
-  </h1>
-  );
-
-const HeadingComponent = () => (
-  <div id="container">
-    {Title()}
-    <Title/>
-    <Title></Title>
-    <h1 className="heading">Namaste React Function Component🚀</h1>
-  </div>
-);
 
 
 
