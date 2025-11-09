cea42e047db4dcb35f7a07be0bbe171305fe9f0e
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-10 02:19:37 +0530

    implimentation pending

--- App.js
+++ App.js
@@ -11,7 +11,9 @@ import ReactDOM from "react-dom/client";
 * - Body
 * - Search
 * - RestaurantContainer
-* - RestaurantCard
+*    - RestaurantCard
+      - Img
+      - Name of res, Star Rating, cuisine, delvery-time 
 * - Footer
 * - Copyrights
 * - Links
@@ -38,21 +40,784 @@ const Header = () => {
   )
 };
 
+       
+
+const RestaurantCard = ( props ) => {
+  const {resData} = props;
+
+  const {cloudinaryImageId,name, cuisines, avgRating, costForTwo, deliveryTime } = resData.info;
+  
+  return (
+    <div className="res-card">
+      <img
+        className="logo"
+        alt="res-logo"
+      src={
+         resData.info.cloudinaryImageId
+         ? "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+         resData.info.cloudinaryImageId
+         : "fallback-image.jpg"
+}
+      />
+
+      <h3>{name}</h3>
+      <h4>{cuisines?.join(", ") || "No cuisines"}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime || resData.sla?.deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+
+
+const resList = [
+{
+info: {
+id: "298332",
+name: "Domino's Pizza",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/8/773ffbc5-3cb9-4c1a-bf2d-ea7dbc66c412_298332.JPG",
+locality: "SAI MANDAR APARTMENT, AMBERNATH (EAST)",
+areaName: "Ambarnath",
+costForTwo: "₹400 for two",
+cuisines: [
+"Pizzas",
+"Italian",
+"Pastas",
+"Desserts"
+],
+avgRating: 3.9,
+parentId: "2456",
+avgRatingString: "3.9",
+totalRatingsString: "1.3K+",
+sla: {
+deliveryTime: 30,
+lastMileTravel: 1.3,
+serviceability: "SERVICEABLE",
+slaString: "25-30 mins",
+lastMileTravelString: "1.3 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 02:59:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "android/static-assets/icons/big_rx.png",
+description: "bolt!"
+},
+{
+imageId: "Rxawards/_CATEGORY-Pizza.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "bolt!",
+imageId: "android/static-assets/icons/big_rx.png"
+}
+},
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Pizza.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "ITEMS",
+subHeader: "AT ₹55"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/dominos-pizza-sai-mandar-apartment-ambernath-east-ambarnath-rest298332",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "509009",
+name: "Burger King",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/58fee667-91a0-4ee4-863e-f051769acaff_509009.jpg",
+locality: "Katrap Road",
+areaName: "Badlapur",
+costForTwo: "₹350 for two",
+cuisines: [
+"Burgers",
+"American"
+],
+avgRating: 4.2,
+parentId: "166",
+avgRatingString: "4.2",
+totalRatingsString: "5.7K+",
+sla: {
+deliveryTime: 39,
+lastMileTravel: 5,
+serviceability: "SERVICEABLE",
+slaString: "35-40 mins",
+lastMileTravelString: "5.0 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 01:00:00",
+opened: true
+},
+badges: { },
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: { },
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "ITEMS",
+subHeader: "AT ₹55"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/burger-king-katrap-road-badlapur-rest509009",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "421681",
+name: "Pizza Hut",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/3f41a313-6966-4ea2-a753-1ef9e17687e7_421681.JPG",
+locality: "Pranali Park",
+areaName: "Badlapur",
+costForTwo: "₹350 for two",
+cuisines: [
+"Pizzas"
+],
+avgRating: 4.2,
+parentId: "721",
+avgRatingString: "4.2",
+totalRatingsString: "3.9K+",
+sla: {
+deliveryTime: 44,
+lastMileTravel: 4.7,
+serviceability: "SERVICEABLE",
+slaString: "40-45 mins",
+lastMileTravelString: "4.7 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 01:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "Rxawards/_CATEGORY-Pizza.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Pizza.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "60% OFF",
+subHeader: "UPTO ₹110"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/pizza-hut-pranali-park-badlapur-rest421681",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "765828",
+name: "Subway",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/12/d50af4dd-8f88-4ff7-bacd-e8b1591e9ed9_765828.jpg",
+locality: "BADLAPUR EAST",
+areaName: "TRIDENT TOWER",
+costForTwo: "₹350 for two",
+cuisines: [
+"sandwich",
+"Salads",
+"wrap",
+"Healthy Food"
+],
+avgRating: 4,
+parentId: "2",
+avgRatingString: "4.0",
+totalRatingsString: "942",
+sla: {
+deliveryTime: 47,
+lastMileTravel: 6.5,
+serviceability: "SERVICEABLE",
+slaString: "45-50 mins",
+lastMileTravelString: "6.5 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 01:00:00",
+opened: true
+},
+badges: { },
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: { },
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "₹150 OFF",
+subHeader: "ABOVE ₹299",
+discountTag: "FLAT DEAL"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/subway-badlapur-east-trident-tower-rest765828",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "81940",
+name: "McDonald's",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/3/254d10d1-82a3-4ce5-9d54-259e554c02f1_81940.JPG",
+locality: "Dombivli East",
+areaName: "Dombivli East",
+costForTwo: "₹400 for two",
+cuisines: [
+"Burgers",
+"Beverages",
+"Cafe",
+"Desserts"
+],
+avgRating: 4.4,
+parentId: "630",
+avgRatingString: "4.4",
+totalRatingsString: "41K+",
+sla: {
+deliveryTime: 49,
+lastMileTravel: 13.1,
+serviceability: "SERVICEABLE",
+slaString: "45-50 mins",
+lastMileTravelString: "13.1 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 03:45:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "Rxawards/_CATEGORY-Burger.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Burger.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "ITEMS",
+subHeader: "AT ₹119"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "4.4",
+ratingCount: "9.9K+"
+},
+source: "GOOGLE",
+sourceIconImageId: "v1704440323/google_ratings/rating_google_tag"
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/mcdonalds-dombivli-east-rest81940",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "423977",
+name: "KFC",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/10/17/d63d8c06-82e1-4f87-98aa-765a454a779d_423977.JPG",
+locality: "Badlapur",
+areaName: "Badlapur",
+costForTwo: "₹400 for two",
+cuisines: [
+"Burgers",
+"Fast Food",
+"Rolls & Wraps"
+],
+avgRating: 4.1,
+parentId: "547",
+avgRatingString: "4.1",
+totalRatingsString: "2.4K+",
+sla: {
+deliveryTime: 38,
+lastMileTravel: 4.6,
+serviceability: "SERVICEABLE",
+slaString: "35-45 mins",
+lastMileTravelString: "4.6 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 01:00:00",
+opened: true
+},
+badges: { },
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: { },
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "ITEMS",
+subHeader: "AT ₹589"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "4.4",
+ratingCount: "1.1K+"
+},
+source: "GOOGLE",
+sourceIconImageId: "v1704440323/google_ratings/rating_google_tag"
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/kfc-badlapur-rest423977",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "183611",
+name: "Faasos - Wraps, Rolls & Shawarma",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/28/fb7dbeee-faf2-417e-91d3-087808a4a2cc_183611.JPG",
+locality: "Krishna builders",
+areaName: "Kalyan",
+costForTwo: "₹200 for two",
+cuisines: [
+"Kebabs",
+"Fast Food",
+"Snacks",
+"American",
+"Healthy Food",
+"Desserts",
+"Beverages"
+],
+avgRating: 4.1,
+parentId: "21809",
+avgRatingString: "4.1",
+totalRatingsString: "5.0K+",
+sla: {
+deliveryTime: 50,
+lastMileTravel: 9.3,
+serviceability: "SERVICEABLE",
+slaString: "45-50 mins",
+lastMileTravelString: "9.3 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 01:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "Rxawards/_CATEGORY-Rolls.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Rolls.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "ITEMS",
+subHeader: "AT ₹89"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/faasos-wraps-rolls-and-shawarma-krishna-builders-kalyan-rest183611",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+info: {
+id: "198914",
+name: "Baskin Robbins - Ice Cream Desserts",
+cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/24/27c3b255-e9a9-4e16-b553-6ec69532dfeb_198914.JPG",
+locality: "Ulhasnagar 2",
+areaName: "Ulhasnagar",
+costForTwo: "₹300 for two",
+cuisines: [
+"Desserts",
+"Ice Cream"
+],
+avgRating: 4.5,
+veg: true,
+parentId: "5588",
+avgRatingString: "4.5",
+totalRatingsString: "177",
+sla: {
+deliveryTime: 51,
+lastMileTravel: 6.9,
+serviceability: "SERVICEABLE",
+slaString: "50-60 mins",
+lastMileTravelString: "6.9 km",
+iconType: "ICON_TYPE_EMPTY"
+},
+availability: {
+nextCloseTime: "2025-11-10 02:00:00",
+opened: true
+},
+badges: {
+imageBadges: [
+{
+imageId: "Rxawards/_CATEGORY-Ice-creams.png",
+description: "Delivery!"
+}
+]
+},
+isOpen: true,
+type: "F",
+badgesV2: {
+entityBadges: {
+imageBased: {
+badgeObject: [
+{
+attributes: {
+description: "Delivery!",
+imageId: "Rxawards/_CATEGORY-Ice-creams.png"
+}
+}
+]
+},
+textBased: { },
+textExtendedBadges: { }
+}
+},
+aggregatedDiscountInfoV3: {
+header: "₹125 OFF",
+subHeader: "ABOVE ₹199",
+discountTag: "FLAT DEAL"
+},
+orderabilityCommunication: {
+title: { },
+subTitle: { },
+message: { },
+customIcon: { }
+},
+differentiatedUi: {
+displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+differentiatedUiMediaDetails: {
+mediaType: "ADS_MEDIA_ENUM_IMAGE",
+lottie: { },
+video: { }
+}
+},
+reviewsSummary: { },
+displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+restaurantOfferPresentationInfo: { },
+externalRatings: {
+aggregatedRating: {
+rating: "--"
+}
+},
+ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+analytics: {
+context: "seo-data-b6ed2bc1-deed-46fd-8244-17a46916a16a"
+},
+cta: {
+link: "https://www.swiggy.com/city/mumbai/baskin-robbins-ice-cream-desserts-2-ulhasnagar-rest198914",
+text: "RESTAURANT_MENU",
+type: "WEBLINK"
+},
+widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+]
+
+
 const Body = () => {
   return (
   <div className="body">
-    <div className="search">Search</div>
+    <div className=" ">Search</div>
     <div className="res-container">
+    {resList.map((restaurant) => (
+      <RestaurantCard resData={restaurant} />
+    ))
       
+    }
+ 
+
     </div>
   </div>
   );
 };
 
 
+
+
+
 const AppLayout = () => {
   return ( <div className="app">
     <Header/>
+    <Body/>
   </div>)
   ;
 };
